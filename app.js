e3b67eeac8288e7e0d02a95a6128b5b677824a53
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-06-23 22:04:41 -0600

    🗄 properly connected html, css, and js to github repo . Also figured out the cors error and properly linked the Comic VIne API to the js

---
+++ app.js
@@ -0,0 +1,5 @@
+
+$.get("https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8")
+.then(function(data) {
+  console.log(data);
+})
