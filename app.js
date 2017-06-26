719f621d57fc02ee9f184d86aeb9d0e162da882d
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-06-26 10:29:45 -0600

    🔗  linked the canvas-drawing-code to my project code: now they function together in the web browser

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-
-$.get("https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8")
-.then(function(data) {
-  console.log(data);
+$(document).ready(function() {
+  drawingApp.init();
+  $.get("https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8")
+  .then(function(data) {
+    console.log(data);
+  })
 })
+
+// 
+// $.get("https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8")
+// .then(function(data) {
+//   console.log(data);
+// })
