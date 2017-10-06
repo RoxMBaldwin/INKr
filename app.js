c593d7da6e3956850fad621ce1fe2356cbe5816f
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-10-06 13:01:01 -0600

    🛀  refactor: 1. moved event functions outside of document.ready 2. renamed 'format' to 'formatJson' (for clarity) and moved baseurl and formatJson into document.ready and set as global vars 3. save repetative jQuery selectors as global vars

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const url = 'https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8&filter=name:'
+const formatJson = '&format=json'
+const $charDropDown = $('#charDropDown')
+const $charRealName = $('#charRealName')
+const $firstAppeared = $('#firstAppeared')
+const $charDescription = $('#charDescription')
+const $charImage = $('.charImage')
+
 $(document).ready(function() {
   drawingApp.init();
 
@@ -9,83 +17,51 @@ $(document).ready(function() {
 
   $('#searchButton').click(searchCharacter)
 
-  function addCharacter(event) {
+})
+
+function addCharacter(event) {
   event.preventDefault()
 
-  $.get('https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8&filter=name:&format=json')
+  $.get(url + formatJson)
   .then(function(data) {
 
     var randomNumber = Math.floor(Math.random() * 100)
-    //console.log(data.results[randomNumber])
     var formalCharName = data.results[randomNumber].name
-    $('#charDropDown').text(formalCharName)
     var realName = data.results[randomNumber].real_name
-    $('#charRealName').text(realName)
     var firstAppeared = data.results[randomNumber].first_appeared_in_issue.name
-    $('#firstAppeared').text(firstAppeared)
     var charDescription = data.results[randomNumber].deck
-    $('#charDescription').text(charDescription)
     var chosenImage = data.results[randomNumber].image.medium_url
-    $('.charImage').attr('src', chosenImage)
 
-    })
+    $charDropDown.text(formalCharName)
+    $charRealName.text(realName)
+    $firstAppeared.text(firstAppeared)
+    $charDescription.text(charDescription)
+    $charImage.attr('src', chosenImage)
 
-  }
+  })
+}
 
 
-  function searchCharacter(event) {
+function searchCharacter(event) {
   event.preventDefault()
 
-  var url = 'https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8&filter=name:'
   var userCharacter = $('.charSearch').val()
-  //console.log(userCharacter)
-  var format = '&format=json'
 
-  $.get(url + userCharacter + format)
+  $.get(url + userCharacter + formatJson)
     .then(displayCharInfo)
   } //searchCharacter function
+
   function displayCharInfo(data){
 
   var formalCharName = data.results[0].name
-      //console.log(formalCharName)
-  $('#charDropDown').text(formalCharName)
   var realName = data.results[0].real_name
-  $('#charRealName').text(realName)
   var firstAppeared = data.results[0].first_appeared_in_issue.name
-  $('#firstAppeared').text(firstAppeared)
   var charDescription = data.results[0].deck
-  $('#charDescription').text(charDescription)
   var chosenImage = data.results[0].image.medium_url
-      //console.log(chosenImage)
-  $('.charImage').attr('src', chosenImage)
+
+  $charDropDown.text(formalCharName)
+  $charRealName.text(realName)
+  $firstAppeared.text(firstAppeared)
+  $charDescription.text(charDescription)
+  $charImage.attr('src', chosenImage)
   }
-}) //closing doc.ready
-
-
-
-
-
-
-
-
-
-// );
-
-
-
-
-// $.get('https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters?api_key=94f09437a4a631a6681888cfeef46dfa08e58ee8&filter=name:'+ characterName + '&format=json')
-// .then(function(data) {
-//    var newImage = data.results[0].image.medium_url
-//    console.log(newImage)
-//    $('.charImage').attr('src', newImage)
-
-//RANDOM MAKE MY OWN ARRAY AND POPULATE IT WITH 10-20 IMAGES **
-
-
-//to push img to html :
-    //  var newImage = data.results[0].image.medium_url
-    //  console.log(newImage)
-    //  $('.charImage').attr('src', newImage)
-
-//******img stc line 499 canvas code******
