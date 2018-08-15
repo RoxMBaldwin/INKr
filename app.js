806a4bfbbe99e3ad9aa73f9129ef7e9be907b1c9
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2018-08-15 14:42:46 -0600

    temporarily fixed svg animation

--- app.js
+++ app.js
@@ -65,3 +65,70 @@ $firstAppeared.text(firstAppeared)
 $charDescription.text(charDescription)
 $charImage.attr('src', chosenImage)
 }
+
+//
+// anime({
+//   targets: '#lineDrawing .image path',
+//   strokeDashoffset: [anime.setDashoffset, 0],
+//   easing: 'easeInOutSine',
+//   rotate: '1turn',
+//   backgroundColor: '#ffffff',
+//   duration: 1500,
+//   loop: false
+// }, console.log('testerr', anime()));
+
+
+  var lineDrawing = anime({
+    targets: '#lineDrawing .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    rotate: '1',
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+
+  anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0]
+});
+
+//
+// var lineDrawing = anime({
+//   targets: '#lineDrawing .image path',
+//   strokeDashoffset: [anime.setDashoffset, 0],
+//   easing: 'easeInOutSine',
+//   rotate: '1turn',
+//   backgroundColor: '#ffffff',
+//   duration: 1500,
+//   loop: false
+// });
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   console.log('innit')
+//   // Get all "navbar-burger" elements
+//   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+//
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
+//
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach(function ($el) {
+//       $el.addEventListener('click', function () {
+//
+//         // Get the target from the "data-target" attribute
+//         var target = $el.dataset.target;
+//         var $target = document.getElementById(target);
+//
+//         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+//         $el.classList.toggle('is-active');
+//         $target.classList.toggle('is-active');
+//
+//       });
+//     });
+//   }
+//
+// });
